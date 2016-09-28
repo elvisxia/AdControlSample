8acc3cddba184f84de13b7d713f419112b0517ce
v-xucxia <v-xucxia@PC-WINFFEE>
2016-09-28 08:57:24 +0800

    Add project files.

---
+++ AdControlSample/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using AdControlSample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdControlSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var viewModel = new ArticleViewModel();
+            this.DataContext = viewModel;
+            
+            pivot.ItemsSource = viewModel.Articles;
+            base.OnNavigatedTo(e);
+        }
+
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Favorite_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AdControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AdControl_ErrorOccurred(object sender, Microsoft.Advertising.WinRT.UI.AdErrorEventArgs e)
+        {
+            
+        }
+
+        private void CloseImage(object sender, TappedRoutedEventArgs e)
+        {
+    
+        }
+    }
+}
---
+++ AdControlSample/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace AdControlSample.ViewModels
+{
+    public class ArticleViewModel
+    {
+        public List<Article> Articles { get; set; }
+        public ArticleViewModel()
+        {
+            Articles = new List<Article>
+            {
+                GenerateArticle(),
+                GenerateArticle(),
+                GenerateArticle(),
+                GenerateArticle(),
+                GenerateArticle(),
+                GenerateArticle()
+            };
+        }
+
+        private Article GenerateArticle()
+        {
+            Article article = new Article();
+            article.UpdatedDate = DateTime.Now.ToString();
+            article.HeadLine = "This is HeadLine";
+            article.FontSize = 18.5;
+            article.HeadlineFontSize = 22.5;
+            article.Abstract = "This is Abstract";
+            article.AbstractFontSize = 18.5;
+            return article;
+        }
+    }
+
+
+
+    public class Article
+    {
+        public String UpdatedDate { get; set; }
+        public String HeadLine { get; set; }
+        public Double FontSize { get; set; }
+        public Double HeadlineFontSize { get; set; }
+        public String Abstract { get; set; }
+
+        public double AbstractFontSize { get; set; }
+
+    }
+}
